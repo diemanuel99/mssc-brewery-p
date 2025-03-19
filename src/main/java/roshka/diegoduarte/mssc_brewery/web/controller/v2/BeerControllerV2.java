c0d864d299d7db64cc903da085016d7dc782046f
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-03-19 10:06:40 -0300

    validation Error handler and return

--- src/main/java/roshka/diegoduarte/mssc_brewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/roshka/diegoduarte/mssc_brewery/web/controller/v2/BeerControllerV2.java
@@ -1,14 +1,21 @@
 package roshka.diegoduarte.mssc_brewery.web.controller.v2;
 
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import roshka.diegoduarte.mssc_brewery.services.v2.BeerServiceV2;
 import roshka.diegoduarte.mssc_brewery.web.model.v2.BeerDtoV2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+@Validated
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
@@ -24,7 +31,7 @@ public class BeerControllerV2 {
     }
 
     @PostMapping //post create new beer
-    public ResponseEntity<HttpHeaders> handlePost(@RequestBody BeerDtoV2 beerDto) {
+    public ResponseEntity<HttpHeaders> handlePost(@Valid @NotNull @RequestBody BeerDtoV2 beerDto) {
         BeerDtoV2 saveDto = beerServiceV2.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         //todo add hostname to url
@@ -33,7 +40,7 @@ public class BeerControllerV2 {
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity<Void> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDto) {
+    public ResponseEntity<Void> handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDtoV2 beerDto) {
         beerServiceV2.updateBeer(beerId,beerDto);
         return new  ResponseEntity<Void> (HttpStatus.NO_CONTENT);
     }
@@ -42,5 +49,16 @@ public class BeerControllerV2 {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeer(@PathVariable("beerId") UUID beerId) {
         beerServiceV2.deleteById(beerId);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException e){
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
